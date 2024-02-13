1eabe88ab94099a44fad95268a948b3f19131cf2
TusharBhosale12 <Tusharbhosale334@gmail.com>
2024-02-13 10:16:25 +0530

    commit

---
+++ new.js
@@ -0,0 +1,14 @@
+console.log("welcome apana college");
+console.log("Tushar Bhosale");
+console.log("i love javascript");
+let age=25
+console.log(age+1);
+let employee= 104;
+console.log(employee);
+const student= {
+    fullName: 'rahul kumar',
+    age: 20,
+    cgpa: 8.5,
+    isPass: true,
+};
+console.log(student);
---
+++ operator.js
@@ -0,0 +1,49 @@
+/*let a=30;
+let b=20;
+console.log(a>=b);
+let age=25;
+if (age >=18){
+    console.log('you can vote');
+}
+else{
+    console.log('you cannot vote');
+}*/
+
+
+// odd or even 
+/*let num=10;
+if(num % 2 === 0){
+    console.log('number is even');
+}
+else{
+    console.log('number is odd');
+}*/
+ /*let age =20;
+ if(age<18){
+    console.log('junior');
+ }else if(age>60){
+console.log('senior');
+ }else{
+    console.log('middle');
+ }*/
+ /*let age=15;
+ let result= age>18 ? "adult" : "not adult";
+ console.log(result);*/
+ /*let num=prompt('hello world')
+ if(num%5===0){
+    console.log(num,'number is multiply of 5');
+ }else{
+    console.log(num,'number is not multiply ogf 5');
+ }*/
+ let num=prompt('enter your marks');
+ if(num>=80 && num<=100){
+    console.log('student grade is A');
+ }else if(num>=70 && num<=89){
+    console.log('student grade is B');
+ }else if(num>=60 && num<=69){
+    console.log('student grade is C');
+ }else if(num>=50 && num<=59){
+    console.log('student grade is D');
+ }else{
+    console.log('student is fail');
+ }
